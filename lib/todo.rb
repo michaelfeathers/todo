65205c86b5fd0a448bf21103cc623f588553acaf
MICHAEL FEATHERS <michaelfeathers@gmail.com>
2023-08-20 16:36:27 -0700

    R moved ToDoUpdater to its own file

--- lib/todo.rb
+++ lib/todo.rb
@@ -4,6 +4,7 @@ require 'common'
 require 'session'
 require 'commands'
 require 'appio'
+require 'todoupdater'
 
 
 class ToDo
@@ -47,49 +48,5 @@ class ToDo
 end
 
 
-class ToDoUpdater
-  def run
-    [[lines_of(TODO_FILE), lines_of(UPDATER_FILE)]]
-      .map {|ts,us| [due(us) + ts, non_due(us)] }
-      .each  do |ts,us| 
-        write_lines(TODO_FILE, ts)
-        write_lines(UPDATER_FILE, us.sort_by {|lines| DateTime.parse(lines.split.first)})
-      end
-  end
-
-  def due us
-    us.select {|e| due?(e)}
-      .map {|e| strip_date(e)} 
-  end
-
-  def non_due us
-     us.reject {|e| due?(e)}
-  end
-
-  def lines_of file_name
-    File.read(file_name).lines
-  end
-
-  def write_lines file_name, lines
-    File.open(file_name, 'w') { |f| f.write(lines.join) }
-  end
-
-  def strip_date line
-    line.split.drop(1).join(" ") + $/
-  end
-
-  def due? line 
-    tokens = line.split
-    return false unless tokens.size > 0
-    day_date(DateTime.parse(tokens.first)) <= day_date(DateTime.now)
-  rescue
-    false
-  end
-end
-
-
 ToDo.new(AppIo.new).run
-
-
-
 
