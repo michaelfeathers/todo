de88e7c4d17e1300349f96424114b530794983a6
MICHAEL FEATHERS <michaelfeathers@gmail.com>
2024-05-14 16:15:23 -0700

    R add specs for Session#surface

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -34,4 +34,39 @@ describe Session do
 
 end
 
+describe 'Session#surface' do
+  before(:each) do
+    @foreground_io = FakeAppIo.new
+    @background_io = FakeAppIo.new
+  end
+
+  it 'does nothing if background list is empty' do
+    expect(@foreground_io).to receive(:append_to_actions).never
+    @session = Session.new(@foreground_io, @background_io)
+    @session.surface(3)
+    @session.save
+  end
+
+  it 'moves the specified number of random tasks from background to foreground' do
+    @background_io.actions_content = "Task 1\nTask 2\nTask 3"
+    @session = Session.new(@foreground_io, @background_io)
+    
+    @session.surface(2)
+    @session.save
+
+    expect(@foreground_io.actions_content.split("\n").size).to eq(2)
+    expect(@background_io.actions_content.split("\n").size).to eq(1)
+  end
+
+  it 'moves all tasks from background to foreground if count exceeds background size' do
+    @background_io.actions_content = "Task 1\nTask 2"
+    @session = Session.new(@foreground_io, @background_io)
+    @session.surface(5)
+    @session.save
+
+    expect(@foreground_io.actions_content.split("\n").size).to eq(2)
+    expect(@background_io.actions_content).to eq("")
+  end
+end
+
 
