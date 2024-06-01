44787f24d4f420b4d5bb5e7ca564a144f1d082c3
MICHAEL FEATHERS <michaelfeathers@gmail.com>
2024-06-01 13:17:44 -0700

    C add specs for move_task_to_random_position_on_other_list

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -34,6 +34,62 @@ describe Session do
 
 end
 
+describe 'Session#move_task_to_random_position_on_other_list' do
+  before(:each) do
+    @f_io = FakeAppIo.new
+    @b_io = FakeAppIo.new
+    @session = Session.new(@f_io, @b_io)
+  end
+
+  context 'when the foreground list is active' do
+    xit 'moves the task at the cursor to a random position on the background list' do
+      @f_io.actions_content = "Task 1\nTask 2\nTask 3\n"
+      @b_io.actions_content = "Task A\nTask B\nTask C\n"
+      @session.list.todo_cursor_set(1)
+
+      expect(@b_io.actions_content.split("\n")).to receive(:insert).with(kind_of(Integer), "Task 2\n").and_call_original
+      expect(@f_io.actions_content.split("\n")).to receive(:delete_at).with(1).and_call_original
+
+      @session.move_task_to_random_position_on_other_list
+    end
+
+    it 'does not modify the lists if the foreground list is empty' do
+      @f_io.actions_content = ""
+      @b_io.actions_content = "Task A\nTask B\nTask C\n"
+
+      expect(@b_io.actions_content.split("\n")).not_to receive(:insert)
+      expect(@f_io.actions_content.split("\n")).not_to receive(:delete_at)
+
+      @session.move_task_to_random_position_on_other_list
+    end
+  end
+
+  context 'when the background list is active' do
+    before { session.switch_lists }
+
+    xit 'moves the task at the cursor to a random position on the foreground list' do
+      @f_io.actions_content = "Task 1\nTask 2\nTask 3\n"
+      @b_io.actions_content = "Task A\nTask B\nTask C\n"
+      @session.list.todo_cursor_set(1)
+
+      expect(@f_io.actions_content.split("\n")).to receive(:insert).with(kind_of(Integer), "Task B\n").and_call_original
+      expect(@b_io.actions_content.split("\n")).to receive(:delete_at).with(1).and_call_original
+
+      @session.move_task_to_random_position_on_other_list
+    end
+
+    xit 'does not modify the lists if the background list is empty' do
+      @f_io.actions_content = "Task 1\nTask 2\nTask 3\n"
+      @b_io.actions_content = ""
+
+      expect(@f_io.actions_content.split("\n")).not_to receive(:insert)
+      expect(@b_io.actions_content.split("\n")).not_to receive(:delete_at)
+
+      @session.move_task_to_random_position_on_other_list
+    end
+  end
+end
+
 describe 'Session#surface' do
   before(:each) do
     @foreground_io = FakeAppIo.new
