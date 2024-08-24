015d4c9dd14a3b62d5642ed67aa6c1cd3f5b5604
MICHAEL FEATHERS <michaelfeathers@gmail.com>
2024-08-24 00:58:05 -0700

    R rename actions to tasks in specs

--- spec/commands/cursor_set_spec.rb
+++ spec/commands/cursor_set_spec.rb
@@ -12,9 +12,9 @@ describe CursorSet do
 
   it 'pages when cursor set off page' do
     pos = TaskList::PAGE_SIZE + 5
-    actions =  50.times.map {|n| "L: task #{n}\n" }
+    tasks   =  50.times.map {|n| "L: task #{n}\n" }
     output  =  50.times.map {|n| "%2d %s L: task %d\n" % [n,n == pos ? "-" : " " ,n] }
-    f_io.tasks_content = actions.join
+    f_io.tasks_content = tasks.join
     CursorSet.new.run("c #{pos}", session)
     session.list.render
     expect(f_io.console_output_content).to eq(RENDER_PAD + output.drop(TaskList::PAGE_SIZE).take(TaskList::PAGE_SIZE).join + "\n")
