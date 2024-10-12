6a4b0ab802c722bd8d36e54308363dc94e05752f
MICHAEL FEATHERS <michaelfeathers@gmail.com>
2024-10-12 08:37:24 -0700

    R change specs to use window rather than render

--- spec/commands/cursor_set_spec.rb
+++ spec/commands/cursor_set_spec.rb
@@ -13,11 +13,11 @@ describe CursorSet do
   it 'pages when cursor set off page' do
     pos = TaskList::PAGE_SIZE + 5
     tasks   =  50.times.map {|n| "L: task #{n}\n" }
-    output  =  50.times.map {|n| "%2d %s L: task %d\n" % [n,n == pos ? "-" : " " ,n] }
+    expected_output  =  50.times.map {|n| [n,n == pos ? "-" : " " , "L: task #{n}\n"] }
     f_io.tasks_content = tasks.join
     CursorSet.new.run("c #{pos}", session)
-    session.render
+    output = session.on_list {|list| list.window }
 
-    expect(f_io.console_output_content).to eq(RENDER_PAD + output.drop(TaskList::PAGE_SIZE).take(TaskList::PAGE_SIZE).join + "\n")
+    expect(output).to eq(expected_output.drop(TaskList::PAGE_SIZE).take(TaskList::PAGE_SIZE))
   end
 end
