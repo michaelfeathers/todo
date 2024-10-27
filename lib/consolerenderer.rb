9cfa6a42e5662b36071b7c309af5bb9570ffe96d
MICHAEL FEATHERS <michaelfeathers@gmail.com>
2024-10-27 09:01:39 +0200

    R alter whitespace

--- lib/consolerenderer.rb
+++ lib/consolerenderer.rb
@@ -5,14 +5,20 @@ class ConsoleRenderer
     list.io.clear_console
     list.io.append_to_console list.description
   
-    lines = list.window.map do |num, cursor, line|
-      "%2d %s%s %s%s" % [num, highlight_on(cursor), cursor, line, highlight_off(cursor)]
-    end.join
+    lines = list.window.map {|fields| format(*fields) }.join
   
     list.io.append_to_console lines + $/
   end
   
   private
+
+  def format task_no, cursor, line
+    "%2d %s%s %s%s" % [task_no, 
+                       highlight_on(cursor), 
+                       cursor, 
+                       line, 
+                       highlight_off(cursor)]
+  end
   
   def highlight_on cursor
     cursor == ' ' ? "" : "\e[41m"
