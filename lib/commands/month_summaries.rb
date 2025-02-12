285110644e70eeeba592c7add8715c82f5b8ff1b
MICHAEL FEATHERS <michaelfeathers@gmail.com>
2025-02-12 15:35:35 -0800

    R move to case statement

--- lib/commands/month_summaries.rb
+++ lib/commands/month_summaries.rb
@@ -10,13 +10,8 @@ class MonthSummaries < Command
 
   def process line, session
     session.on_list do |list|
-      tokens = line.split
-      case tokens.count
-      when 1
-        list.todo_month_summaries
-      when 2
-        list.todo_month_summaries(tokens[1].to_i)
-      end
+      month = line.split[1].to_i if line.split.size > 1
+      list.todo_month_summaries(month)
     end
   end
 
