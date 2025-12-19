ab1d09c4535508e2c128d2f72a0cec2a51c003c2
MICHAEL FEATHERS <michaelfeathers@gmail.com>
2025-12-19 07:50:52 -0800

    R cleanup command requires

--- lib/commands.rb
+++ lib/commands.rb
@@ -1,4 +1,4 @@
-# This file is kept as a compatibility layer
-# All commands have been moved to individual files in the lib/commands directory
+# Auto-loads all command files from the commands directory
+require_relative 'command'
 
-# If there are any remaining commands to implement, they should be moved to their own files.
+Dir[File.join(__dir__, 'commands', '*.rb')].sort.each { |file| require file }
