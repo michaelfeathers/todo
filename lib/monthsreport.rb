72163fb9a0e0029dedee46b80dee0c4b2f3f9f4f
MICHAEL FEATHERS <michaelfeathers@gmail.com>
2025-12-22 08:35:41 -0800

    Fixed spelling mistake

--- lib/monthsreport.rb
+++ lib/monthsreport.rb
@@ -15,7 +15,7 @@ class MonthsReport
   COLUMNS = [
              ["Life",  ->(tasks) { tasks.L.count } ],
              ["Work",  ->(tasks) { tasks.R.count + tasks.D.count } ],
-             ["DRW",   ->(tasks) { tasks.D.count } ],
+             ["DRD",   ->(tasks) { tasks.D.count } ],
              ["W %",   ->(tasks) { tasks.R.percent_of(tasks) + tasks.D.percent_of(tasks)} ],
              ["Total", ->(tasks) { tasks.count } ]
             ]
